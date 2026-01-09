ac4e1c2f74ab014f9d6381f99bf7a03c5670682b
junior-gerome <jnguepmanyo@gmail.com>
2026-01-09 17:50:21 +0100

    mise a jour de ParentMapper, StudentMapper et AnneeScolaireMapper avec l'ajout de @beanMapping(nullValuePropertyMappingStrategy=NullValuePropertyMappingStrategy.IGNORE) pour gere les mises a jour partielles si l'utilisateur modifie un dto et laisse le reste sans changement que cela n'ecrase pas le reste en base de donnee

--- src/main/java/com/school/management/mappers/ClasseRoomMapper.java
+++ src/main/java/com/school/management/mappers/ClasseRoomMapper.java
@@ -2,8 +2,11 @@ package com.school.management.mappers;
 
 import java.util.List;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.school.management.dto.ClasseRoomDTO;
 import com.school.management.model.ClasseRoom;
@@ -11,18 +14,19 @@ import com.school.management.model.ClasseRoom;
 @Mapper(componentModel = "spring", uses = {SectionMapper.class, TeacherMapper.class, AnneeScolaireMapper.class})
 public interface ClasseRoomMapper extends EntityMapper<ClasseRoomDTO, ClasseRoom> {
   
-  // @Mapping(target = "section", ignore = true)
-  // @Mapping(target = "teacher", ignore = true)
-  // @Mapping(target = "anneeScolaire", ignore = true)
   ClasseRoomDTO toDto(ClasseRoom entity);
 
   @Mapping(target = "createdAt", ignore = true)
   @Mapping(target = "updatedAt", ignore = true)
-  // @Mapping(source = "sectionId", target = "section.id")
-  // @Mapping(source = "teacherId", target = "teacher.id")
-  // @Mapping(source = "anneeScolaireId", target = "anneeScolaire.id")
   @Mapping(target = "actif", ignore = true)
   ClasseRoom toEntity(ClasseRoomDTO dto);
+
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "updatedAt", ignore = true)
+  @Mapping(target = "actif", ignore = true)
+  @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+  void updateEntityFromDto(ClasseRoomDTO dto, @MappingTarget ClasseRoom entity);
 
   List<ClasseRoomDTO> toDto(List<ClasseRoom> entityList);
 
